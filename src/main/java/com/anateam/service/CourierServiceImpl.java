9ea722741e5b0d8314bca40fe24301d8646200da
demiurg39 <chiryagov2014@gmail.com>
2025-11-10 10:01:58 +0600

    fix CourierServiceImpl

--- src/main/java/com/anateam/service/CourierServiceImpl.java
+++ src/main/java/com/anateam/service/CourierServiceImpl.java
@@ -1,10 +1,12 @@
-package com.anateam.service.impl;
+package com.anateam.service;
 
 import com.anateam.dto.CourierProfileDto;
 import com.anateam.dto.GpsCoordinatesDto;
 import com.anateam.entity.Courier;
+import com.anateam.entity.CourierStatus;
+import com.anateam.entity.GpsCoordinates;
+import com.anateam.entity.User;
 import com.anateam.repository.CourierRepository;
-import com.anateam.service.CourierService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,34 +18,46 @@ public class CourierServiceImpl implements CourierService {
 
     private final CourierRepository courierRepository;
 
+    private CourierProfileDto toCourierProfileDto(Courier courier) {
+        User user = courier.getUser();
+
+        return new CourierProfileDto(
+            user.getId(), user.getFullName(), user.getPhoneNumber(),
+            courier.getStatus().name(), courier.getVehicleType().name());
+    }
+
     @Override
     public Page<CourierProfileDto> findAll(Pageable pageable) {
-        return courierRepository.findAll(pageable)
-                .map(CourierProfileDto::new)
+        return courierRepository.findAll(pageable).map(
+            this::toCourierProfileDto);
     }
 
     @Override
     public CourierProfileDto findDtoById(Integer courierId) {
-        Courier courier = courierRepository.findById(courierId)
-                .orElseThrow(() -> new RuntimeException("Courier not found"));
-        return new CourierProfileDto(courier);
-
+        Courier courier = courierRepository.findById(courierId).orElseThrow(
+            () -> new RuntimeException("Courier not found"));
+        return toCourierProfileDto(courier);
     }
+
     @Override
     public CourierProfileDto updateStatus(Integer courierId, String status) {
-        Courier courier = courierRepository.findbyId(courierId)
-                .orElseThrow(() -> new RuntimeException("Courier not found"));
+        Courier courier = courierRepository.findById(courierId).orElseThrow(
+            () -> new RuntimeException("Courier not found"));
 
-        courier.setStatus(status);
-        courierRepository.save(courier);
+        courier.setStatus(CourierStatus.valueOf(status.toUpperCase()));
+        return toCourierProfileDto(courierRepository.save(courier));
     }
-    @Override
-    public void updateLocation(Integer courierId, GpsCoordinatesDto locationDto) {
-        Courier courier = courierRepository.findById(courierId)
-                .orElseThrow(() -> new RuntimeException("Courier not found"));
 
-        courier.setLatitude(locationDto.getLatitude());
-        courier.setLongitude(locationDto.getLongitude());
+    @Override
+    public void updateLocation(Integer courierId,
+                               GpsCoordinatesDto locationDto) {
+        Courier courier = courierRepository.findById(courierId).orElseThrow(
+            () -> new RuntimeException("Courier not found"));
+
+        GpsCoordinates newCoordinates =
+            new GpsCoordinates(locationDto.latitude(), locationDto.longitude());
+
+        courier.setCurrentCoordinates(newCoordinates);
 
         courierRepository.save(courier);
     }
