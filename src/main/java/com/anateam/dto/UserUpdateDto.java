99d1e2315e1e2d36b3bb9f5769d1644f58ee7aff
demiurg39 <chiryagov2014@gmail.com>
2025-11-29 00:35:01 +0600

    add size validation

--- src/main/java/com/anateam/dto/UserUpdateDto.java
+++ src/main/java/com/anateam/dto/UserUpdateDto.java
@@ -1,12 +1,15 @@
 package com.anateam.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Size;
 
 public record UserUpdateDto(
     @Schema(description = "New full name", example = "Jane Doe")
+    @Size(min = 2, max = 100)
     String fullName,
 
     @Schema(description = "New phone number", example = "88005553535")
+    @Size(min = 10, max = 15)
     String phoneNumber
 ) {}
 
