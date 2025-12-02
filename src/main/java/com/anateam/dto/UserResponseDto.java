d8b1cad3d0823955630b6da85a529dc0ecbfb7d2
demiurg39 <chiryagov2014@gmail.com>
2025-12-02 08:00:13 +0600

    add swagger docs to userResponse

--- src/main/java/com/anateam/dto/UserResponseDto.java
+++ src/main/java/com/anateam/dto/UserResponseDto.java
@@ -1,10 +1,20 @@
 package com.anateam.dto;
 
-// TODO: add annotations for fields
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record UserResponseDto(
+    @Schema(description = "User ID", example = "1")
     Integer id,
+
+    @Schema(description = "Full Name", example = "John Doe")
     String fullName,
+
+    @Schema(description = "Phone Number", example = "+1234567890")
     String phoneNumber,
+
+    @Schema(description = "User Role", example = "CUSTOMER")
     String role,
+
+    @Schema(description = "Account Creation Date", example = "2023-10-01T12:00:00")
     String createdAt
 ) {}
