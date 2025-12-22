d9ffb9b94e13df9296cd84f11f6a80cc45f7c0c2
demiurg39 <chiryagov2014@gmail.com>
2025-12-22 22:38:01 +0600

    format code

--- src/main/java/com/anateam/dto/SendCodeDto.java
+++ src/main/java/com/anateam/dto/SendCodeDto.java
@@ -4,5 +4,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 public record SendCodeDto(
-        @Schema(description = "Phone number to send verification code", example = "996555123456", requiredMode = Schema.RequiredMode.REQUIRED) @NotBlank String phoneNumber) {
-}
+        @Schema(description = "Phone number to send verification code", example = "996555123456", requiredMode = Schema.RequiredMode.REQUIRED)
+        @NotBlank String phoneNumber
+) {}
--- src/main/java/com/anateam/dto/VerifyCodeDto.java
+++ src/main/java/com/anateam/dto/VerifyCodeDto.java
@@ -4,7 +4,9 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 public record VerifyCodeDto(
-        @Schema(description = "Phone number", example = "996555123456", requiredMode = Schema.RequiredMode.REQUIRED) @NotBlank String phoneNumber,
+        @Schema(description = "Phone number", example = "996555123456", requiredMode = Schema.RequiredMode.REQUIRED)
+        @NotBlank String phoneNumber,
 
-        @Schema(description = "Verification code received via SMS", example = "1234", requiredMode = Schema.RequiredMode.REQUIRED) @NotBlank String code) {
+        @Schema(description = "Verification code received via SMS", example = "1234", requiredMode = Schema.RequiredMode.REQUIRED)
+        @NotBlank String code) {
 }
